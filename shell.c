ed92f66998ba9056599ffb40659ba2cf974e4282
Selith <selithrubasingha@gmail.com>
2025-12-30 11:40:37 +0530

    wrote the lsh_loop function which takes care of loop logic

--- shell.c
+++ shell.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
+void lsh_loop(void){
+    char* line;
+    char** args;
+    int status ; 
 
+    do{
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+
+
+    }while (status);
+}
 
 int main(int argc, char** argv ){
     
