5ac70c03ac76c5c44d2f4c74dce36920885979d5
Selith <selithrubasingha@gmail.com>
2025-12-30 11:30:51 +0530

    wrote the main structure of the main method

--- shell.c
+++ shell.c
@@ -1,5 +1,13 @@
+#include <stdlib.h>
 
 
-int main(){
-    return 0 ;
+int main(int argc, char** argv ){
+    
+    //load config files
+
+    //run the loop
+    lsh_loop();
+    
+    //perform any shutdown cleanup
+    return EXIT_SUCCESS ;// fancy way of saying return 0
 }
