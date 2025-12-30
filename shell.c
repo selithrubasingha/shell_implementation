d0eedc930f97cb1f44f80776b5fc9975aac5b362
Selith <selithrubasingha@gmail.com>
2025-12-30 12:15:43 +0530

    wrote the lsh_rea_line method

--- shell.c
+++ shell.c
@@ -1,4 +1,46 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#define LSH_RL_BUFSIZE 1024
+
+char* lsh_read_line(void){
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char* buffer = malloc(sizeof(char)*bufsize);
+    int c ;
+    
+    if (!buffer){
+        fprintf(stderr, "lsh: allocation error\n");
+        exit( EXIT_FAILURE);
+    }
+
+    while (1){
+        c = getchar();
+
+        if (c==EOF || c=='\n'){
+            buffer[position] = '\0';
+            return buffer;
+        }else{
+            buffer[position] = c;
+        }
+
+        buffer[position] = c;
+
+        position++;
+
+
+        if (position>=bufsize){
+            bufsize+=LSH_RL_BUFSIZE;
+
+            buffer = realloc(buffer,bufsize);
+
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
 void lsh_loop(void){
     char* line;
     char** args;
