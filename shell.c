dd744232554000d66e81cb8c39020255afb63a00
Selith <selithrubasingha@gmail.com>
2025-12-31 11:03:11 +0530

    wrote the lsh launch method

--- shell.c
+++ shell.c
@@ -1,5 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int lsh_launch(char **args){
+    pid_t pid , wpid ;
+    int status;
+
+    pid = fork();
+
+    if (pid ==0 ){
+        if (execvp(args[0],args) == -1){
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }else if (pid<0){
+        perror("lsh");
+    }else{
+
+        do{
+            wpid = waitpid(pid,&status,WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
 
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
